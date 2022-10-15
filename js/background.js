292bef884d1931b97dbd71e2fd4d98f9c0ac7546
maxoflondon <maxoflondon@gmail.com>
2022-10-15 17:01:03 +0100

    Initial commit

---
+++ js/background.js
@@ -0,0 +1,59 @@
+/**
+ * Registers action that is called when user clicks on addon button (browser action)
+ */
+chrome.action.onClicked.addListener(tab => {
+    const patterns = [
+        /\/dp\/([A-Z0-9]+)/,
+        /\/product\/([A-Z0-9]+)/
+    ]
+    const tabUrl = new URL(tab.url)
+
+    /**
+     * Applies regex patterns to url string to get product id. Stores match object or null if no matches.
+     */
+    const productId = patterns.reduce((prev, regex) => {
+        return prev || tab.url.match(regex)
+    }, null)
+
+    let notification
+    if(productId) {
+        notification = {
+            type: 'basic',
+            title: chrome.i18n.getMessage('notifOkTitle'),
+            message: `${tabUrl.protocol}//${tabUrl.host}/dp/${productId[1]}`,
+            iconUrl: '../images/img128.png'
+        }
+    } else {
+        notification = {
+            type: 'basic',
+            title: chrome.i18n.getMessage('notifFailTitle'),
+            message: '',
+            iconUrl: '../images/img128.png'
+        }
+    }
+    
+    /**
+     * Copies passed url into clipboard in context of active tab
+     * 
+     * @param {String} url 
+     */
+    function sendToClipboard(url) {
+        const copyFrom = document.createElement("textarea");
+        copyFrom.textContent = url;
+        document.body.appendChild(copyFrom);
+        copyFrom.select();
+        document.execCommand('copy');
+        document.body.removeChild(copyFrom);
+    }
+
+    if('Notification' in this){
+        chrome.notifications.create('', notification);
+        chrome.scripting.executeScript(
+            {
+              target: {tabId: tab.id},
+              func: sendToClipboard,
+              args: [notification.message],
+            }
+        );
+    }
+});
